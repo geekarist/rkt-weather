4f2eacb2c67d9e4cf4d5825eb0fa09a7a3d770ca
geekarist <geekarist@users.noreply.github.com>
2022-03-03 13:29:08 +0100

    Creating windows

--- hello.rkt
+++ hello.rkt
@@ -2,8 +2,6 @@
 
 (define frame (new frame% [label "Example"]))
 
-(send frame show #t)
-
 (define msg (new message%
                  [parent frame]
                  [label "No events so far..."]))
@@ -13,4 +11,34 @@
      [label "Click here!"]
      [callback (lambda (button event)
                  (send msg set-label "Button click"))])
-     
+
+(define my-canvas% 
+  (class canvas%
+    (define/override (on-event event)
+      (send msg set-label "Canvas mouse"))
+    (define/override (on-char event)
+      (send msg set-label "Canvas keyboard"))
+    (super-new)))
+
+(new my-canvas% [parent frame])
+
+(new button%
+     [parent frame]
+     [label "Pause"]
+     [callback
+      (lambda (button event) (sleep 5))])
+
+(define panel (new horizontal-panel% [parent frame]))
+
+(new button% [parent panel]
+     [label "Left"]
+     [callback (lambda (button event)
+                 (send msg set-label "Left click"))])
+
+(new button% [parent panel]
+     [label "Right"]
+     [callback (lambda (button event)
+                 (send msg set-label "Right click"))])
+
+(send frame show #t)
+
