b6c8bdbf9b498179d0cda4a5b691aa92da40fda3
geekarist <geekarist@users.noreply.github.com>
2022-03-11 08:59:26 +0100

    Extract weather module

---
+++ weather.rkt
@@ -0,0 +1,65 @@
+#lang racket
+
+(provide weather-init weather-view weather-update)
+
+(require racket/gui/easy)
+
+(define huge-font (font "Arial" 48 #:weight 'medium))
+(define large-font (font "Arial" 18 #:weight 'medium))
+(define normal-font (font "Arial" 12 #:weight 'medium))
+(define small-font (font "Arial" 8 #:weight 'medium))
+(define small-font-bold (font "Arial" 8 #:weight 'bold))
+
+(define weather-init
+  (hash 'city "Berrias-et-Casteljau"
+        'region "Auvergne-Rhône-Alpes"
+        'icon "☽"
+        'text-desc "Clear"
+        'update-time "Updated as of 12:34"
+        'temp-real "16°"
+        'temp-felt "Feels like 9°"
+        'wind "Wind ↑ 1 mph"
+        'visibility "Visibility 12.4 mi"
+        'pressure "Barometer 1016.00 mb"
+        'humidity "Humidity 77%"
+        'dew-point "Dew Point 5°"))
+
+(define (weather-view state)
+  (render
+   (window
+    (spacer)
+    [vpanel
+     #:margin '(16 16)
+     (text (hash-ref state 'city) #:font large-font)
+     (text (hash-ref state 'region) #:font normal-font)
+     (hpanel
+      #:margin '(0 0) 
+      (spacer)
+      (text (hash-ref state 'icon) #:font huge-font)
+      (text (hash-ref state 'temp-real) #:font huge-font)
+      (choice '("C" "F")
+              (λ (arg)
+                (writeln (string-append "Choice: " arg)))) 
+      (spacer))
+     (text (hash-ref state 'text-desc) #:font normal-font)
+     (text (hash-ref state 'update-time) #:font small-font-bold)
+     (hpanel
+      (spacer)
+      (text (hash-ref state 'temp-felt) #:font small-font)
+      (text " · " #:font small-font)
+      (text (hash-ref state 'wind) #:font small-font)
+      (text " · " #:font small-font)
+      (text (hash-ref state 'visibility) #:font small-font)
+      (spacer))
+     (hpanel
+      (spacer)
+      (text (hash-ref state 'pressure) #:font small-font)
+      (text " · " #:font small-font)
+      (text (hash-ref state 'humidity) #:font small-font)
+      (text " · " #:font small-font)
+      (text (hash-ref state 'dew-point) #:font small-font)
+      (spacer))]
+    (spacer))))
+
+(define (weather-update state message dispatch)
+  (writeln 'TODO))
