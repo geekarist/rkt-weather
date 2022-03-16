201d60d388859ce2f2d164049c5eb092ba9c799e
geekarist <geekarist@users.noreply.github.com>
2022-03-16 09:00:51 +0100

    Extract weather-update

--- main.rkt
+++ main.rkt
@@ -27,8 +27,17 @@
       (λ (state-hash)
         (hash-ref state-hash 'result)))))))
 
-(define (weather-update state-hash msg)
-  "TODO")
+(define (weather-update current-state-hash msg-vec)
+  (define msg-key (vector-ref msg-vec 0))
+  (define msg-val (vector-ref msg-vec 1))
+  (cond
+    [(equal? msg-key 'change-query)
+     (define new-query msg-val)
+     (hash-set current-state-hash 'query new-query)]
+    [(equal? msg-key 'execute-search)
+     (define query (hash-ref current-state-hash 'query))
+     (define new-result (format "TODO: search result for ~a" query))
+     (hash-set current-state-hash 'result new-result)]))
 
 (define @state-global
   (@ weather-init))
@@ -37,22 +46,10 @@
  (weather-view
   @state-global
   (λ (msg-vec)
-    (define msg-key (vector-ref msg-vec 0))
-    (define msg-val (vector-ref msg-vec 1))
     (define current-state-hash (obs-peek @state-global))
-    (cond
-      [(equal? msg-key 'change-query)
-       (obs-update!
-        @state-global
-        (λ (current-state-hash)
-          (define new-query msg-val)
-          (define new-state-hash
-            (hash-set current-state-hash 'query new-query))
-          new-state-hash))]
-      [(equal? msg-key 'execute-search)
-       (define query (hash-ref current-state-hash 'query))
-       (define new-result (format "TODO: search result for ~a" query))
-       (obs-update! @state-global
-                    (λ (current-state-hash)
-                      (hash-set current-state-hash 'result new-result)))]))))
+    (define new-state-hash
+      (weather-update current-state-hash msg-vec))
+    (obs-update! @state-global
+                 (λ (_current-state-hash)
+                   new-state-hash)))))
 
