13e2d314ea25ee84b7bbcd4dfcc631ec4c603bfb
geekarist <geekarist@users.noreply.github.com>
2022-03-17 08:23:17 +0100

    Extract weather program

--- main.rkt
+++ main.rkt
@@ -4,35 +4,7 @@
 
 (require racket/gui/easy)
 (require racket/gui/easy/operator)
-
-(define weather-init
-  (hash
-   'query ""
-   'result "Please type a search string and click 'Search'"
-   ))
-
-(define (weather-view get-state-prop dispatch)
-  (define (on-change-query _event query)
-    (dispatch (vector-immutable 'change-query query)))
-  (define (on-execute-search)
-    (dispatch (vector-immutable 'execute-search null)))
-  (window
-   (vpanel
-    (input "" on-change-query)
-    (button "Search" on-execute-search)
-    (text (get-state-prop 'result)))))
-
-(define (weather-update current-state-hash msg-vec)
-  (define msg-key (vector-ref msg-vec 0))
-  (define msg-val (vector-ref msg-vec 1))
-  (cond
-    [(equal? msg-key 'change-query)
-     (define new-query msg-val)
-     (hash-set current-state-hash 'query new-query)]
-    [(equal? msg-key 'execute-search)
-     (define query (hash-ref current-state-hash 'query))
-     (define new-result (format "TODO: search result for ~a" query))
-     (hash-set current-state-hash 'result new-result)]))
+(require "weather.rkt")
 
 (define (obs-map-hash-ref @state key)
   (obs-map @state
@@ -53,4 +25,6 @@
                    (λ (_current-state-hash)
                      new-state-hash))))))
 
-(runtime weather-init weather-view weather-update)
+(runtime (hash-ref weather-program 'init)
+         (hash-ref weather-program 'view)
+         (hash-ref weather-program 'update))
---
+++ weather.rkt
@@ -0,0 +1,40 @@
+#lang racket
+
+(require racket/gui/easy)
+(require racket/gui/easy/operator)
+
+(define weather-init
+  (hash
+   'query ""
+   'result "Please type a search string and click 'Search'"
+   ))
+
+(define (weather-view get-state-prop dispatch)
+  (define (on-change-query _event query)
+    (dispatch (vector-immutable 'change-query query)))
+  (define (on-execute-search)
+    (dispatch (vector-immutable 'execute-search null)))
+  (window
+   (vpanel
+    (input "" on-change-query)
+    (button "Search" on-execute-search)
+    (text (get-state-prop 'result)))))
+
+(define (weather-update current-state-hash msg-vec)
+  (define msg-key (vector-ref msg-vec 0))
+  (define msg-val (vector-ref msg-vec 1))
+  (cond
+    [(equal? msg-key 'change-query)
+     (define new-query msg-val)
+     (hash-set current-state-hash 'query new-query)]
+    [(equal? msg-key 'execute-search)
+     (define query (hash-ref current-state-hash 'query))
+     (define new-result (format "TODO: search result for ~a" query))
+     (hash-set current-state-hash 'result new-result)]))
+
+(define weather-program
+  (hash 'init weather-init
+        'view weather-view
+        'update weather-update))
+
+(provide weather-program)
