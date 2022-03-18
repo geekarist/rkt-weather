623aa93b5274e3084a4aa299ecbafc0d051e1972
geekarist <geekarist@users.noreply.github.com>
2022-03-18 23:31:37 +0100

    Extract specific update functions

--- weather.rkt
+++ weather.rkt
@@ -5,7 +5,7 @@
 
 (define weather-init
   (hash 'query ""
-	'result "Please type a search string and click 'Search'"))
+        'result "Please type a search string and click 'Search'"))
 
 (define (weather-view get-state-prop dispatch)
   (define (on-change-query _event query)
@@ -18,23 +18,29 @@
     (button "Search" on-execute-search)
     (text (get-state-prop 'result)))))
 
+(define (update-on-execute-search current-state-hash msg-val)
+  (define query (hash-ref current-state-hash 'query))
+  (define new-result (format "TODO3: search result for ~a" query))
+  (define new-state
+    (hash-set current-state-hash 'result new-result))
+  (define new-effect (lambda (fx-key fx-val)
+                       (printf "~a: ~a~n" fx-key fx-val)))
+  new-state)
+
+(define (update-on-change-query current-state-hash msg-val)
+  (define new-query msg-val)
+  (define new-state 
+    (hash-set current-state-hash 'query new-query))
+  new-state)
+
 (define (weather-update current-state-hash msg-vec)
   (define msg-key (vector-ref msg-vec 0))
   (define msg-val (vector-ref msg-vec 1))
   (cond
     [(equal? msg-key 'change-query)
-     (define new-query msg-val)
-	 (define new-state 
-	   (hash-set current-state-hash 'query new-query))
-	 new-state]
+     (update-on-change-query current-state-hash msg-val)]
     [(equal? msg-key 'execute-search)
-     (define query (hash-ref current-state-hash 'query))
-     (define new-result (format "TODO3: search result for ~a" query))
-	 (define new-state
-	   (hash-set current-state-hash 'result new-result))
-	 (define new-effect (lambda (fx-key fx-val)
-						  (printf "~a: ~a~n" fx-key fx-val)))
-	 new-state]))
+     (update-on-execute-search current-state-hash msg-val)]))
 
 (define weather-program
   (hash 'init weather-init
